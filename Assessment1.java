2055ed068b8db745fc5fab9e794d086a57866d17
santoshfaculty1 <santoshfaculty1@gmail.com>
2014-10-31 18:03:32 +0530

    Create Assessment1.java

---
+++ Assessment1.java
@@ -0,0 +1,13 @@
+package com.java.test1;
+
+public class Fibonacci {
+	//Fibonacci - Fibonacci numbers or Fibonacci sequence are the numbers in the following integer sequence
+	//	1,1,2,3,5,8,13,21,34,55,89,144
+	//Write a Java program that should print Fibonacci sequence for a given number. 
+	
+	public int fibonacci(int n)  {
+		//Your code shoudl go here.
+	}
+	
+	
+}
